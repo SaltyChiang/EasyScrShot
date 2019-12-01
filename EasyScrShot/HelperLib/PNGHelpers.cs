c108809599e50a27414eef7b2a49b462cd493f8f
SaltyChiang <jxyzjchina@gmail.com>
2019-12-01 20:25:33 +0800

    Minor update.

--- EasyScrShot/HelperLib/PNGHelpers.cs
+++ EasyScrShot/HelperLib/PNGHelpers.cs
@@ -3,11 +3,54 @@ using System.IO;
 using System.Windows.Forms;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Drawing;
-using System.Drawing.Imaging;
 
 namespace EasyScrShot.HelperLib
 {
+    public class OptiPNG
+    {
+        internal static class OptimizationLevel
+        {
+            public const string Level0 = "-o0",
+                Level1 = "-o1",
+                Level2 = "-o2",
+                Level3 = "-o3",
+                Level4 = "-o4",
+                Level5 = "-o5",
+                Level6 = "-o6",
+                Level7 = "-o7",
+                Level8 = "-o8",
+                Level9 = "-o9";
+        };
+
+        private string Arguments = "-nx";
+
+        internal OptiPNG()
+        {
+            Arguments += " " + OptimizationLevel.Level1;
+        }
+        internal OptiPNG(string optimizationLevel)
+        {
+            Arguments += " " + optimizationLevel;
+        }
+
+        internal void LosslessCompress(string inputFile, string outputFile)
+        {
+            Arguments += " " + "\"" + inputFile + "\"";
+            Arguments += " -out " + "\"" + outputFile + "\"";
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = "./optipng";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardInput = false;
+            process.StartInfo.RedirectStandardOutput = false;
+            process.StartInfo.RedirectStandardError = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.Arguments = Arguments;
+            process.Start();
+            process.WaitForExit();
+            process.Close();
+        }
+    }
+
     public static class PNGHelpers
     {
         private static int completeCount { get; set; }
@@ -22,7 +65,7 @@ namespace EasyScrShot.HelperLib
         {
             int i, j;
             fileCount = fileList.Length;
-            //PreCompress();
+            PreCompress();
             if (fileCount < threadsMaxCount)
                 threadsMaxCount = fileCount;
 
